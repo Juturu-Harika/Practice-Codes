a2bf28171ecc8ebb3d7d3a13be78624ba178b89a
Juturu-Harika <juturu.harika@bounteous.com>
2025-05-24 19:05:44 +0530

    Update valid_brackets.py

--- valid_brackets.py
+++ valid_brackets.py
@@ -1,3 +1,10 @@
+'''Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input 
+string is valid.
+A string is valid if:
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+3. Every close bracket has a corresponding open bracket of the same type.
+'''
 def valid_brackets(l):
     stack=[]
     for i in l:
